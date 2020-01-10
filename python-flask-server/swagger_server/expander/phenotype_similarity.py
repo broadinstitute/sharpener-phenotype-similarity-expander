75b40cb190359d7bd87b9606c069ec62df1defb7
vdancik <vdancik@broadinstitute.org>
2020-01-10 16:58:51 -0500

    update transformer info to version 1.3

--- python-flask-server/swagger_server/expander/phenotype_similarity.py
+++ python-flask-server/swagger_server/expander/phenotype_similarity.py
@@ -30,14 +30,14 @@ def expand(query: TransformerQuery):
         Execute this expander, find all genes correlated to query genes.
     """
     controls = {control.name: control.value for control in query.controls}
-    print(controls)
+
     threshold = float(controls[THRESHOLD])
     gene_list = []
     genes = {}
     for gene in query.genes:
         gene_list.append(gene)
         genes[gene.gene_id] = gene
-    print ("len =",len(gene_list))
+
     psg = PhenotypicallySimilarGenes(query.genes, threshold)
     results = psg.results.to_dict('records') if len(psg.results)>0 else []
 
